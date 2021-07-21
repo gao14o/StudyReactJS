70bcba2056d9111c74d90d490d8d856cac9cf2fb
gao14o <74162537+gao14o@users.noreply.github.com>
2021-07-21 00:15:20 -0400

    Update Navbar2.js

--- Navbar2.js
+++ Navbar2.js
@@ -2,6 +2,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The AG'S Blog</h1>
+        
             <div className="links">
                 <a href="/">Home</a>
         
