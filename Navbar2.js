fa2bcb41ffa39ff75b7963d4cab2d0e7c1eca021
gao14o <74162537+gao14o@users.noreply.github.com>
2021-07-26 14:35:31 -0400

    Update Navbar2.js

--- Navbar2.js
+++ Navbar2.js
@@ -9,6 +9,7 @@ const Navbar = () => {
         
                 <a href="/create" style={{
                     color:"white",
+        
                     backgroundColor:'#f1356d',
                     borderRadius:'8px'
         
